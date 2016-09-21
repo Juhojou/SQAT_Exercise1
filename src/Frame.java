241742a48a1afc64a6ddd4d0c17cc0f01bac052d
somename <someemail>
2016-09-21 16:16:53 +0300

    besouro automatic message

--- src/Frame.java
+++ src/Frame.java
@@ -22,7 +22,7 @@ public class Frame {
 	
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		return (getFirstThrow() == 10 );
+		return (getFirstThrow() == 10 && isSpare() == true);
 	}
 	
 	//return whether a frame is a spare or not
