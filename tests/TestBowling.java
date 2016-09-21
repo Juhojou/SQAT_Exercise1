f48b4403549c9133251d39e5b029502c9253e512
somename <someemail>
2016-09-21 15:14:45 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -5,12 +5,6 @@ import org.junit.Test;
 
 public class TestBowling {
 	
-	@Before
-	public void beforeEachTest(){
-		BowlingGame game = new BowlingGame();
-	}
-	
-
 	@Test
 	public void test_firstFrameNoPins() throws BowlingException{
 		Frame frame = new Frame(0,0);
@@ -35,6 +29,16 @@ public class TestBowling {
 			game.addFrame(frame);
 		}
 		assertEquals(0, game.score());
+	}
+	
+	@Test
+	public void test_allFramesGotOnePoint() throws BowlingException{
+		BowlingGame game = new BowlingGame();
+		for (int i=0; i<10; i++) {
+			Frame frame = new Frame(1,0);
+			game.addFrame(frame);
+		}
+		assertEquals(10, game.score());
 	}
 
 }
