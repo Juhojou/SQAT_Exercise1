589f630a407b240abf40984d6f5284df5fc9fdc3
somename <someemail>
2016-09-21 16:00:42 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -91,11 +91,13 @@ public class TestBowling {
 	@Test
 	public void test_FirstFourFramesAreStrikeOthersGotOnePoint() throws BowlingException{
 		BowlingGame game = new BowlingGame();
-		for (int i=0; i<3; i++) {
-			Frame Frame = new Frame(10,0);
+		for (int i=0; i<9; i++) {
+			Frame Frame = new Frame(5,5);
 			game.addFrame(Frame);
 		}
-		assertEquals(20, game.score());
+		Frame LastFrame = new Frame(1,0);
+		game.addFrame(LastFrame);
+		assertEquals(132, game.score());
 	}
 
 }
