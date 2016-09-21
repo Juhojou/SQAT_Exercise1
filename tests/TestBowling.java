6b8c27c9997a36201d6879aca58dc3df7bb6f65d
somename <someemail>
2016-09-21 14:50:03 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -7,7 +7,7 @@ public class TestBowling {
 	@Test (expected =BowlingException.class)
 	public void test_firstFrameNoPins() {
 		Frame frame = new Frame(0,0);
-		assertEquals(0, frame.score());
+		assertEquals("BowlingExpection", 0, frame.score());
 	}
 
 }
