4a9d99c14f41b53e537d45224dd83a9037af8fce
somename <someemail>
2016-09-21 16:00:09 +0300

    besouro automatic message

--- tests/TestBowling.java
+++ tests/TestBowling.java
@@ -95,7 +95,7 @@ public class TestBowling {
 			Frame Frame = new Frame(10,0);
 			game.addFrame(Frame);
 		}
-		for (int k=0; k<7; k++) {
+		for (int k=3; k<9; k++) {
 			Frame Frame = new Frame(1,0);
 			game.addFrame(Frame);
 		}
